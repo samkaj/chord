9f4937c71d9058732085917b6f1bf57e74c91a0f
danryg <danryg@student.chalmers.se>
2023-12-09 20:08:07 +0100

    Adding test command

--- chord/cli.go
+++ chord/cli.go
@@ -67,6 +67,8 @@ func (c *CLI) storeFile(path string) {
 		fmt.Fprintf(os.Stderr, "No path supplied\n")
 		return
 	}
+	fmt.Printf("Sending file")
+	TLSSend(c.Node.Successors[0], []byte("Hello from client"))
 	fmt.Fprintf(os.Stdout, "StoreFile(%s)\n", path)
 }
 
